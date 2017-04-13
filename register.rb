60d9d28a72677abff5f7957e64f654209d664d59
o-jill <nobaokix@gmail.com>
2017-04-13 11:53:17 +0900

    sending a mail to new user.

--- register.rb
+++ register.rb
@@ -3,7 +3,10 @@
 
 #!/usr/bin/ruby
 
+require 'yaml'
 require 'digest/sha2'
+require 'mail'
+
 require './common_ui.rb'
 require "./userinfofile.rb"
 
@@ -73,8 +76,35 @@ def register_screen(header, title, name, params)
     print "Registered successfully.<BR>",
     "username:", username, "<BR>",
     "password:****<BR>",
-    "email address:", email1, "<BR>"
+    "email address:", email1, "<BR>",
+    "Registration mail has been sent.<BR>"
 
     CommonUI::HTMLfoot()
+
+    message = "Dear ", username, "\n",
+      "Your information has been registed successfully as below.\n\n",
+      "User name: ", username, "\n",
+      "Password: ", password1, "\n",
+      "E-mail address: ", email1, "\n\n",
+      "* Please delete this email if you believe you are not the intended recipient.\n",
+      "* Please do not respond to this auto-generated email."
+
+    dlvcfg = YAML.load_file('./config/mail.yaml')
+    mail = Mail.new do
+      from    dlvcfg['mailaddress']
+      to      email1
+      subject "Welcome to Wash Crus!"
+      body    message
+    end
+    mail.delivery_method(dlvcfg['type'],
+      address:        dlvcfg['address'],
+      port:           dlvcfg['port'],
+      domain:         dlvcfg['domain'],
+      authentication: dlvcfg['authentication'],
+      user_name:      dlvcfg['user_name'],
+      password:       dlvcfg['password']
+    )
+p mail
+    mail.deliver
   end
 end
