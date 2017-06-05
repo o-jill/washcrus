61315774f8eda26478e72a2935f7c4f6890f9953
o-jill <nobaokix@gmail.com>
2017-06-05 09:07:34 +0900

    added entrance.js.

---
+++ js/entrance.js
@@ -0,0 +1,14 @@
+function check_loggedin()
+{
+  var loggedin = document.getElementById('isloggedin').value;
+  var signup = document.getElementById('signup');
+  var signin = document.getElementById('signin');
+  if (loggedin == 1) {
+    signup.style.backgroundColor = 'transparent';
+    signin.style.backgroundColor = 'transparent';
+  } else {
+    signup.style.backgroundColor = 'green';
+    signin.style.backgroundColor = 'blue';
+  }
+}
+check_loggedin();
