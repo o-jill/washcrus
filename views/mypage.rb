abd575975e1c513a6c40914a540fce392640e63c
o-jill <nobaokix@gmail.com>
2017-08-07 12:03:37 +0900

    added your stats to your 'my page'.

--- views/mypage.rb
+++ views/mypage.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'unindent'
 
 require './file/taikyokufile.rb'
+require './file/userinfofile.rb'
 require './views/common_ui.rb'
 
 def put_err_sreen(header, title, name, errmsg)
@@ -11,6 +12,35 @@ def put_err_sreen(header, title, name, errmsg)
   CommonUI::HTMLmenu(name)
   puts errmsg
   CommonUI::HTMLfoot()
+end
+
+def put_stats(uid)
+  udb = UserInfoFile.new
+  udb.read
+
+  wl = udb.stats[uid]
+  wl[4] = wl[0] + wl[1]
+  wl[5] = wl[2] + wl[3]
+
+  ttl = [wl[0] + wl[2], wl[1] + wl[3], 0]
+  ttl[2] = ttl[0] + ttl[1]
+
+  print <<-STATS.unindent
+    <table>
+    <tr>
+     <th>総合成績</th>
+     <td>#{ttl[0]}勝#{ttl[1]}敗 (#{ttl[2].zero? ? '0.000' : ttl[0].quo(ttl[2]).round(3)})</td>
+    </tr>
+    <tr>
+     <th>先手成績</th>
+     <td>#{wl[0]}勝#{wl[1]}敗 (#{wl[4].zero? ? '0.000' : wl[0].quo(wl[4]).round(3)})</td>
+    </tr>
+    <tr>
+     <th>後手成績</th>
+     <td>#{wl[2]}勝#{wl[3]}敗 (#{wl[5].zero? ? '0.000' : wl[2].quo(wl[5]).round(3)})</td>
+    </tr>
+    </table>
+    STATS
 end
 
 def put_taikyokuchu(uid)
@@ -85,6 +115,8 @@ def mypage_screen(header, title, name, userinfo)
   CommonUI::HTMLHead(header, title)
   CommonUI::HTMLmenuLogIn(name)
 
+  put_stats(userinfo.user_id)
+  print '<HR>'
   put_taikyokuchu(userinfo.user_id)
   print '<HR>'
   put_taikyokurireki(userinfo.user_id)
