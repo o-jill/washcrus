00c80a2cf2a2ac2e1710bb71720d645fa86cdd5d
o-jill <nobaokix@gmail.com>
2017-08-25 17:59:36 +0900

    added sfenlogging.

---
+++ file/sfenstore.rb
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+# Sfenを保存するファイル管理クラス
+class SfenStore
+  # DIRPATH = './taikyoku/'.freeze
+  # STOREFILE = '/sfenlog.txt'.freeze
+
+  def initialize(sfenpath)
+    @path = sfenpath  # DIRPATH + @id + STOREFILE
+  end
+
+  attr_reader :path
+
+  def add(line)
+    File.open(@path, 'a') do |file|
+      file.flock File::LOCK_EX
+      file.puts line
+    end
+  # 例外は小さい単位で捕捉する
+  rescue SystemCallError => e
+    puts "class=[#{e.class}] message=[#{e.message}] in write"
+  rescue IOError => e
+    puts "class=[#{e.class}] message=[#{e.message}] in write"
+  end
+end
