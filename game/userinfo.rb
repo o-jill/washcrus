57b6336e57abe333d52d8cdc82b7606cd7054da7
o-jill <nobaokix@gmail.com>
2017-10-02 10:32:59 +0900

    added admin info to session data.

--- game/userinfo.rb
+++ game/userinfo.rb
@@ -1,5 +1,9 @@
 # -*- encoding: utf-8 -*-
 
+require 'rubygems'
+require 'unindent'
+
+require './file/adminconfigfile.rb'
 require './file/userinfofile.rb'
 
 #
@@ -12,46 +16,70 @@ class UserInfo
     @user_name = name
     @user_email = addr
     @win_lose = { swin: 0, slose: 0, gwin: 0, glose: 0 }
+    @admin = id.nil? ? false : check_admin
   end
 
   attr_accessor :visitcount, :user_id, :user_name, :user_email, :win_lose
+  attr_reader :admin
 
+  # read data from session hash object.
   def readsession(session)
     # keys for session must be Strings, not symbols.
     @visitcount = session['count'].to_i + 1
     @user_id = session['user_id']
     @user_name = session['user_name']
     @user_email = session['user_email']
+    @admin = session['adminright'] || false
   end
 
+  # generate data hash object.
   def hashsession
     {
       # keys for session must be Strings, not symbols.
       'count' => @visitcount, 'user_id' => @user_id,
-      'user_name' => @user_name, 'user_email' => @user_email
+      'user_name' => @user_name, 'user_email' => @user_email,
+      'adminright' => @admin
     }
   end
 
+  # check if data is valid.
   def invalid?
     user_id.nil? || user_id.length.zero? || user_name.nil? \
     || user_name.length.zero? || user_email.nil? || user_email.length.zero?
   end
 
+  # check if exist
   def exist_indb
     db = UserInfoFile.new
     db.read
     db.exist_id(@user_id)
   end
 
-  def dump
-    print "userid:#{@user_id}username:#{@user_name}",
-          "useremail:#{@user_email}visitcount:#{@visitcount}"
+  # check and set if administrator.
+  def check_admin
+    ac = AdminConfigFile.new
+    ac.read
+    @admin = ac.exist?(@user_id)
   end
 
+  # put data
+  def dump
+    print "userid:#{@user_id},username:#{@user_name}" \
+          "useremail:#{@user_email},visitcount:#{@visitcount}," \
+          "admin:#{@admin}"
+
+  end
+
+  # put data in html table format.
   def dumptable
-    print "<TABLE><TR><TD>userid</TD><TD>#{@user_id}</TD></TR>",
-          "<TR><TD>username</TD><TD>#{@user_name}</TD></TR>",
-          "<TR><TD>useremail</TD><TD>#{@user_email}</TD></TR>",
-          "<TR><TD>visitcount</TD><TD>#{@visitcount}</TD></TR></TABLE>"
+    print <<-TABLEDATA.unindent
+      <table>
+       <tr><td>userid</td><td>#{@user_id}</td></tr>
+       <tr><td>username</td><td>#{@user_name}</td></tr>
+       <tr><td>useremail</td><td>#{@user_email}</td></tr>
+       <tr><td>visitcount</td><td>#{@visitcount}</td></tr>
+       <tr><td>admin</td><td>#{@admin}</td></tr>
+      </table>
+      TABLEDATA
   end
 end
--- views/userlist.rb
+++ views/userlist.rb
@@ -1,19 +1,15 @@
 # -*- encoding: utf-8 -*-
 
-require './game/userinfo.rb'
 require './file/adminconfigfile.rb'
 require './file/userinfofile.rb'
+require './game/userinfo.rb'
 require './views/common_ui.rb'
 
 #
 # userlist画面
 #
 def userlist_screen(header, title, name, userinfo)
-  ac = AdminConfigFile.new
-  ac.read
-  unless ac.exist?(userinfo.user_id)
-    return puts "Content-Type: text/plain;\n\nERR_NOT_ADMIN"
-  end
+  return puts "Content-Type: text/plain;\n\nERR_NOT_ADMIN" unless userinfo.admin
 
   userdb = UserInfoFile.new
   userdb.read
