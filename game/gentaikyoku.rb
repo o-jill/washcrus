aaef7228db2dc0b8696ff13bf890b7a6bb9f47ea
o-jill <nobaokix@gmail.com>
2017-12-19 13:23:39 +0900

    beautified code.

--- game/gentaikyoku.rb
+++ game/gentaikyoku.rb
@@ -12,46 +12,25 @@ class GenTaikyokuData
     @id = taikyoku.gid
   end
 
-  attr_reader :taikyoku, :id
+  # attr_reader :taikyoku, :id
+
+  # ファイルをモード666で作成
+  #
+  # @param path ファイルパス
+  def touch(path)
+    File.open(path, 'w', 0o666).close
+  end
 
   # 対局に必要なファイル群の生成
   def generate
     FileUtils.mkdir(taikyoku.taikyokupath, mode: 0o777)
 
-    File.open(taikyoku.matchinfopath, 'w', 0o666).close
-    # matchfile = taikyoku.matchinfopath
-    # FileUtils.touch(matchfile)
-    # FileUtils.chmod(0o666, matchfile)
+    touch(taikyoku.matchinfopath)
 
-    File.open(taikyoku.chatpath, 'w', 0o666).close
-    # chatfile = taikyoku.chatpath
-    # FileUtils.touch(chatfile)
-    # FileUtils.chmod(0o666, chatfile)
+    touch(taikyoku.chatpath)
 
-    File.open(taikyoku.kifupath, 'w', 0o666).close
-    # kifufile = taikyoku.kifupath
-    # FileUtils.touch(kifufile)
-    # FileUtils.chmod(0o666, kifufile)
+    touch(taikyoku.kifupath)
 
-    File.open(taikyoku.sfenpath, 'w', 0o666).close
-    # sfenfile = taikyoku.sfenpath
-    # sfenUtils.touch(sfenfile)
-    # sfenUtils.chmod(0o666, sfenfile)
-
-    # print <<-GEN_LOG
-    # FileUtils.mkdir(#{directory}, { :mode => 0777 })
-    #
-    # matchfile = directory+MATCHFILE
-    # FileUtils.touch(#{matchfile})
-    # FileUtils.chmod(0666, #{matchfile})
-    #
-    # chatfile = directory+CHATFILE
-    # FileUtils.touch(#{chatfile})
-    # FileUtils.chmod(0666, #{chatfile})
-    #
-    # kifufile = directory+KIFUFILE
-    # FileUtils.touch(#{kifufile})
-    # FileUtils.chmod(0666, #{kifufile})
-    # GEN_LOG
+    touch(taikyoku.sfenpath)
   end
 end
