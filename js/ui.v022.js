7e6860d9139edaf0c64f8631d4cf23a208822455
o-jill <nobaokix@gmail.com>
2017-12-24 00:16:35 +0900

    changed from px-base to vw-base when portrait.

--- js/ui.v022.js
+++ js/ui.v022.js
@@ -150,7 +150,7 @@ function Naraberu_putkoma(el, koma, n123)
   if (fn.length === 0) {
     el.innerHTML = '&nbsp;';//'<img src="./image/dot16.png" width="100%" height="100%" />';
   } else {
-    el.innerHTML = '<img width="100%" height="100%" src="./image/'
+    el.innerHTML = '<img src="./image/'
                   + fn + '.png">';
   }
 }
