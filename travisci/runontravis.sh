fe23a44fb8aa360e25faa2683ea22c5c7ecd9f9b
o-jill <nobaokix@gmail.com>
2018-11-30 16:59:53 +0900

    fixed setup procedure.

--- travisci/runontravis.sh
+++ travisci/runontravis.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # a script for travis ci
 
+cp config/settings.yaml.sample config/settings.yaml
+cp config/mail.yaml.sample config/mail.yaml
+cp config/signature.txt.sample config/signature.txt
 rake gen_token
 bundle exec rake add_admin << ADMININFO
 admin1
@@ -15,17 +18,7 @@ rake init
 
 if [ "${TRAVIS_BUILD_TYPE}" = "test" ]; then
   echo "let us TEST !!"
-  cp config/settings.yaml.sample config/settings.yaml
-  cp config/mail.yaml.sample config/mail.yaml
-  cp config/signature.txt.sample config/signature.txt
   bundle exec rspec
   bundle exec rubocop
 else
-#   bundle exec rake add_admin << ADMININFO
-# admin1
-# admin1@example.com
-# admin1@example.com
-# admin
-# admin
-# ADMININFO
 fi
