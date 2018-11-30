4228e578c228a98f387c12ac638adc40e693e9d0
o-jill <nobaokix@gmail.com>
2018-11-30 15:53:33 +0900

    added a script for travisci and use it.

---
+++ travisci/runontravis.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# a script for travis ci
+
+if [ "${TRAVIS_BUILD_TYPE}" = "test" ]; then
+  echo "let us TEST !!"
+  bundle exec rspec
+  bundle exec rubocop
+else
+  cp config/settings.yaml.sample config/settings.yaml
+  cp config/mail.yaml.sample config/mail.yaml
+  cp config/signature.txt.sample config/signature.txt
+  rake gen_token
+  bundle exec rake add_admin << ADMININFO
+    admin1
+    admin1@example.com
+    admin1@example.com
+    admin
+    admin
+    ADMININFO
+  rake init
+fi
