41f135a1cf0b4149bc68d9dddac1a6ab749edc10
o-jill <nobaokix@gmail.com>
2019-02-28 19:14:38 +0900

    simplified code.

--- js/searchform.js
+++ js/searchform.js
@@ -1,26 +1,32 @@
+function checkname(id)
+{
+  var name;
+  name = document.getElementById(id);
+  if (name.value.length < 4) {
+    return 1;
+  }
+  return 0;
+}
+
+function checkdate(id)
+{
+  var date;
+  date = document.getElementById(id);
+  if (!date.value.match(/\d{4}[/-](0[1-9]|1[0-2])[/-](0[1-9]|1\d|3[0-1])/)) {
+    return 1;
+  }
+  return 0;
+}
+
 function check_form()
 {
   var nmismatch = 0;
 
-  var name;
-  name = document.getElementById('player1');
-  if (name.value.length < 4) {
-    ++nmismatch;
-  }
-  name = document.getElementById('player2');
-  if (name.value.length < 4) {
-    ++nmismatch;
-  }
+  nmismatch += checkname('player1');
+  nmismatch += checkname('player2');
 
-  var date;
-  date = document.getElementById('time_frame_from');
-  if (!date.value.match(/\d{4}[/-](0[1-9]|1[0-2])[/-](0[1-9]|1\d|3[0-1])/)) {
-    ++nmismatch;
-  }
-  date = document.getElementById('time_frame_to');
-  if (!date.value.match(/\d{4}[/-](0[1-9]|1[0-2])[/-](0[1-9]|1\d|3[0-1])/)) {
-    ++nmismatch;
-  }
+  nmismatch += checkdate('time_frame_from');
+  nmismatch += checkdate('time_frame_to');
 
   if (nmismatch === 4) {
     document.getElementById('errmsg').innerText = 'please put some information!!';
