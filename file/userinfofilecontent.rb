b395232d8fedf9caa14da67d9afc69baaa6c5593
o-jill <nobaokix@gmail.com>
2021-07-25 11:56:24 +0900

    support ruby2.3 syntax check.

--- file/userinfofilecontent.rb
+++ file/userinfofilecontent.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 # frozen_string_literal: true
-
+    
 require 'digest/sha2'
 require 'openssl'
 require 'unindent'
--- views/userlist.rb
+++ views/userlist.rb
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+# frozen_string_literal: true
 
 require './file/adminconfigfile.rb'
 require './file/userinfofile.rb'
