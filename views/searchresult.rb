28c6b52d7c01b7283bd05e903cc40f5220de0790
o-jill <nobaokix@gmail.com>
2021-07-25 12:52:59 +0900

    beautified code.

--- views/searchresult.rb
+++ views/searchresult.rb
@@ -152,7 +152,9 @@ class SearchResultScreen
 
   # 検索結果の出力
   def print_result(res)
-    return '<p>not found ...</p>' unless res && res.size.nonzero?
+    # nil or zero
+    return '<p>not found ...</p>' unless res
+    return '<p>not found ...</p>' if res.size.zero?
 
     rows = res.map do |game|
       resultrow(**game)
