3b9f5ad676606d72aeaf515b5c3a425938929b14
o-jill <nobaokix@gmail.com>
2021-09-02 13:22:35 +0900

    separated lock().

---
+++ file/mylock.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# lock() module
+
+# ファイルlock用
+module MyLock
+  # usage:
+  # lock('aaaa.lock') do
+  #   do_something
+  # end
+  def lock(lockfn)
+    Timeout.timeout(10) do
+      File.open(lockfn, 'w') do |file|
+        begin
+          file.flock(File::LOCK_EX)
+          yield
+        ensure
+          file.flock(File::LOCK_UN)
+        end
+      end
+    end
+  rescue Timeout::Error
+    raise AccessDenied.new('timeout')
+  end
+end
