e94a6d2ece533fedbf8ed3ad2dfb5d4894bd4ec2
o-jill <nobaokix@gmail.com>
2021-11-06 23:22:45 +0900

    caught up tweetbtn value.

--- views/gamehtml.rb
+++ views/gamehtml.rb
@@ -139,7 +139,7 @@ class GameHtml
 
   def tweetbtn
     stg = Settings.instance
-    return '' unless stg.value['tweetbtn']
+    return '' if stg.value['tweetbtn'] != 'show'
 
     '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" ' \
     'class="twitter-share-button" data-show-count="false">Tweet</a>' \
