d71a2f73bcc763b7597b3f0844f87c59d6041154
o-jill <nobaokix@gmail.com>
2021-12-10 21:36:18 +0900

    added rouge. added some configs for markdown.

--- views/news.rb
+++ views/news.rb
@@ -4,9 +4,14 @@
 require 'rubygems'
 require 'unindent'
 require 'redcarpet'
+require 'rouge/plugins/redcarpet'
 require './file/pathlist.rb'
 # require './game/userinfo.rb'
 require './views/common_ui.rb'
+
+class NewsRender < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
 
 #
 # NEWS画面
@@ -21,7 +26,26 @@ class NewsScreen
 
   # newsの出力
   def put_news
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    render = NewsRender.new(
+      # filter_html: true,
+      hard_wrap: true
+      # with_toc_data: true
+    )
+    extensions = {
+      autolink: true,
+      disble_indented_code_blocks: true,
+      # escape_html: true,
+      fenced_code_blocks: true,
+      highlight: true,
+      no_intra_emphasis: true,
+      # quote: true,
+      space_after_headers: true,
+      strikethrough: true,
+      superscript: true,
+      tables: true,
+      underline: true
+    }
+    markdown = Redcarpet::Markdown.new(render, extensions)
     msg = markdown.render(File.read(PathList::NEWSFILE, encoding: 'utf-8'))
 
     puts <<-NEWS_INFO.unindent
